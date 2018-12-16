8324bb9f02ed566b6cc59fbe967bf290426732b8
fabienne <fabienne@fidzup.com>
2018-12-16 15:42:20 +0100

    add index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
 
-  namespace :v1 do
-    post '/translations', to: 'translations#create'
+  namespace :v1, defaults: { format: :json } do
+    post "/translations", to: "translations#create"
+    get "/translations", to: "translations#index"
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
