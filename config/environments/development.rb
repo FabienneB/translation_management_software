d07f337dd5a2ef3523a986040e571d8ea431d70a
Elodie <elodie@MacBook-Air-de-Elodie.local>
2018-12-16 15:20:24 +0100

    set up cors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'localhost:3000', '127.0.0.1:3000'
+      resource '*', headers: :any
+    end
+  end
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
